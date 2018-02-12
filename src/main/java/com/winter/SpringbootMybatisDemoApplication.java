f43ed6598b137f37b2d18676b5f7effc1caca2eb
rtywei <85694291@qq.com>
2018-02-12 21:43:20 +0800

    添加说明

--- src/main/java/com/winter/SpringbootMybatisDemoApplication.java
+++ src/main/java/com/winter/SpringbootMybatisDemoApplication.java
@@ -4,6 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 启动类
+ * 
+ * @author 85694
+ *
+ */
 @SpringBootApplication
 @MapperScan("com.winter.mapper")
 public class SpringbootMybatisDemoApplication {
